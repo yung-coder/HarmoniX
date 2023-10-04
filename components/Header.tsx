014c5b2b65ce0b2a6e4c7feab1662c4eca119e71
yung-coder <chandel.sanjeev5@gmail.com>
2023-10-04 23:37:32 +0530

    layout done

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeadProps {
   children: React.ReactNode;
@@ -14,10 +15,15 @@ interface HeadProps {
 const Header: React.FC<HeadProps> = ({ children, className }) => {
   const router = useRouter();
 
-  const handleLogout = async () => {};
+  const handleLogout = async () => { };
 
   return (
-    <div className={twMerge(`h-fit bg-gradient-to-b from-emerald-800 p-6` , className)}>
+    <div
+      className={twMerge(
+        `h-fit bg-gradient-to-b from-emerald-800 p-6`,
+        className,
+      )}
+    >
       <div className="w-full mb-4 flex items-center justify-between">
         <div className="hidden md:flex gap-x-2 items-center">
           <button
@@ -41,7 +47,26 @@ const Header: React.FC<HeadProps> = ({ children, className }) => {
             <BiSearch className="text-black" szie={20} />
           </button>
         </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button
+                onClick={() => { }}
+                className="bg-transparent text-neutral-300 font-medium"
+              >
+                Sign Up
+              </Button>
+            </div>
+
+            <div>
+              <Button onClick={() => { }} className="bg-white px-6 py-2">
+                Login
+              </Button>
+            </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   );
 };
