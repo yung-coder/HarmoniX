8b6961ee1ca1ddff92c1ed88cebb10cc501e49df
yung-coder <chandel.sanjeev5@gmail.com>
2023-10-15 14:12:23 +0530

    auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Figtree } from "next/font/google";
 import Sidebar from "@/components/Sidebar";
 import SupabaseProvider from "@/Provider/SupabaseProvider";
 import UserProvider from "@/Provider/UserProvider";
+import ModalProvider from "@/Provider/ModalProvider";
+import ToasterProvider from "@/Provider/ToasterProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -20,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
+            <ModalProvider />
             <Sidebar>
               {children}
             </Sidebar>
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,11 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 interface HeadProps {
   children: React.ReactNode;
@@ -14,8 +19,20 @@ interface HeadProps {
 
 const Header: React.FC<HeadProps> = ({ children, className }) => {
   const router = useRouter();
+  const authModal = useAuthModal();
 
-  const handleLogout = async () => { };
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    router.refresh();
+
+    if (error) {
+      toast.error(error.message);
+    } else {
+      toast.success("Logged out!");
+    }
+  };
 
   return (
     <div
@@ -48,22 +65,44 @@ const Header: React.FC<HeadProps> = ({ children, className }) => {
           </button>
         </div>
         <div className="flex justify-between items-center gap-x-4">
-          <>
-            <div>
-              <Button
-                onClick={() => { }}
-                className="bg-transparent text-neutral-300 font-medium"
-              >
-                Sign Up
-              </Button>
-            </div>
+          {user
+            ? (
+              <div className="flex gap-x-4 items-center">
+                <Button
+                  onClick={handleLogout}
+                  className="bg-white px-6 py-2"
+                >
+                  Logout
+                </Button>
+                <Button
+                  className="bg-white"
+                  onClick={() => router.push("/account")}
+                >
+                  <FaUserAlt />
+                </Button>
+              </div>
+            )
+            : (
+              <>
+                <div>
+                  <Button
+                    onClick={authModal.onOpen}
+                    className="bg-transparent text-neutral-300 font-medium"
+                  >
+                    Sign Up
+                  </Button>
+                </div>
 
-            <div>
-              <Button onClick={() => { }} className="bg-white px-6 py-2">
-                Login
-              </Button>
-            </div>
-          </>
+                <div>
+                  <Button
+                    onClick={authModal.onOpen}
+                    className="bg-white px-6 py-2"
+                  >
+                    Login
+                  </Button>
+                </div>
+              </>
+            )}
         </div>
       </div>
       {children}
