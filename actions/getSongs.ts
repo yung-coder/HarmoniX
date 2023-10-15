06d2cc321c5ff4c2d34c20b78d1c496d787cacd0
yung-coder <chandel.sanjeev5@gmail.com>
2023-10-15 20:39:12 +0530

    song fetching

---
+++ actions/getSongs.ts
@@ -0,0 +1,22 @@
+import { Song } from "@/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+const getSongs = async (): Promise<Song[]> => {
+  const supbase = createServerComponentClient({
+    cookies: cookies,
+  });
+
+  const { data, error } = await supbase
+    .from("songs")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.log(error);
+  }
+
+  return (data as any) || [];
+};
+
+export default getSongs;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      "sydbxcrxwhsrvqlgqjak.supabase.co"
+    ]
+  }
+}
 
 module.exports = nextConfig
